3bc944558891bd4c56218894b7c3bce912aa0323
Jun Luo <luojunmoo@gmail.com>
2018-12-09 10:39:43 +0800

    Prevent Unnecessary Update for Modal Component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,24 +2,25 @@ import React from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientsSummary = Object.entries(props.ingredients)
-      .map(ele =>(<li key={ele[0]}>
-                    <span style={{textTransform: 'capitalize'}}>{ele[0]}</span>: {ele[1]}
-                  </li>));
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </>
-  );
-};
+export default class OrderSummary extends React.Component {
 
-export default orderSummary;
+  render() {
+    const ingredientsSummary = Object.entries(this.props.ingredients)
+        .map(ele =>(<li key={ele[0]}>
+                      <span style={{textTransform: 'capitalize'}}>{ele[0]}</span>: {ele[1]}
+                    </li>));
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientsSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </>
+    );
+    }
+};
