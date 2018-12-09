8805828dbf1d39129950d157d3900cb5112ea794
Jun Luo <luojunmoo@gmail.com>
2018-12-09 10:50:00 +0800

    Move Layout from components folder to hoc folder

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 export default class layout extends React.Component {
   state = {
