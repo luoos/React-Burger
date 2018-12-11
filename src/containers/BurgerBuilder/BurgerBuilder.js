21fc52cee8ed74f5afe46629f515a9e87aba4e26
Jun Luo <luojunmoo@gmail.com>
2018-12-11 22:46:16 +0800

    Send Order to Json Server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -69,7 +70,23 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('Continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'John Doe',
+        address: {
+          street: 'Test street',
+          zipCode: '11111',
+          country: 'China'
+        },
+        email: 'john@test.com'
+      },
+      deliveryMethod: 'express'
+    }
+    axios.post('/orders', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render () {
