63f4e05908046af913caa91249884c87f82105bd
Jun Luo <luojunmoo@gmail.com>
2018-12-20 13:50:00 +0800

    Config Webpack to Redirect 404s
    REF: https://tylermcginnis.com/react-router-cannot-get-url-refresh/

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve('dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   module: {
     rules: [
